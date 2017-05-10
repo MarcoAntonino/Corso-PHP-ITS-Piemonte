5b6bf1d89a4a3d2a2df01aaa34edc97bd50ff80f
Marco Antonino <antoninom@AULE.IT>
2017-05-10 14:35:09 +0200

    aadsa

---
+++ EserciSwolley/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Inserisci il codice</title>
+  </head>
+  <body>
+
+    <h1>Questa è la pagina in cui inserire codici</h1>
+
+    <form class="" action="index.html" method="post">
+      <label>Scrivi qui il codice</label>
+      <input type="text" name="codice"/>
+      <button type="submit" name="" value="Submit">verifica codice</button>
+
+    </form>
+
+  </body>
+</html>
---
+++ EserciSwolley/verificacodice.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Controllo codice</title>
+  </head>
+  <body>
+
+    <?php
+
+    if (isset($_POST[])) {
+      # code...
+    }
+
+
+
+
+
+
+
+
+     ?>
+
+  </body>
+</html>
